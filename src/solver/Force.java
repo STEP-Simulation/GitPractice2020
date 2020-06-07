c60440841d98e4310aa66cf1c25118746cb0b990
Keidxb610 <s1911055@s.tsukuba.ac.jp>
2020-06-07 16:28:13 +0900

    コメントの削除, return の位置変更

--- src/solver/Force.java
+++ src/solver/Force.java
@@ -5,13 +5,7 @@ package solver;
  * */
 public class Force {
 
-	/**時間[s]の配列*/
-	private double[] t;
 
-	/**力[N]の配列*/
-	private double[] F;
-
-	private double f;
 	private int n;
 	private double[] t_array;
 	private double[] F_array;
@@ -23,10 +17,7 @@ public class Force {
 	 * */
 	public Force(double[] t_array, double[] F_array) {
 		n = t_array.length;
-		t = new double[n + 1];
-		F = new double[n + 1];
 		this.t_array = t_array;
-		//System.out.println(Arrays.toString(t_array));
 		this.F_array = F_array;
 
 	}
@@ -39,30 +30,23 @@ public class Force {
 	 * @return f 時刻t[s]における力[N]
 	 * */
 	public double value(double t) {
-		//System.out.println("kitayo!");
-		//System.out.println("t=");
-		//System.out.println(t);
+		double f = 0;
 		if(t >= t_array[0] && t <= t_array[n-1]) {
-			//System.out.println("here");
 			for(int i = 0; i < n; i++) {
 				if(t < t_array[i]) {
-					//System.out.println("aaaaaaaa");
-					//System.out.println(F_array[i-1]);
-					//System.out.println(F_array[i]);
 					f = F_array[i-1] + (F_array[i] - F_array[i-1]) * (t - t_array[i-1]) / ((t_array[i]) - t_array[i-1]);
-					//System.out.println("f =");
-					//System.out.println(f);
+					return f;
 				}
 			}
 			if(t >= t_array[n-1]) {
 				f = F_array[n-1];
+				return f;
 			}
 		}
 		else {
 			f = 0;
+			return f;
 		}
-
-		return f;
 
 	}
 
