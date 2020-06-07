02b889085cc9e19291adc8364b7d271b0bad9b40
ryota511 <ryotasumitomo0511@gmail.com>
2020-06-07 17:46:28 +0900

    solverメソッドの実装

--- src/solver/Solver.java
+++ src/solver/Solver.java
@@ -20,6 +20,7 @@ public class Solver {
 
 	private Force F;
 	private double dt;
+	private int n;
 
 	/**
 	/**
@@ -36,7 +37,7 @@ public class Solver {
 	/**mainからもらった値をSolverにセットする
 	 * n=2のステップ数の時、n=0,1,2の３つの値が必要になった→n+1
 	 * new doubleの後ろは欲しい値の数(ここではステップをn踏むので、n+1になる)
-	 *
+	 *ステップ数はtの値によらない
 	 * */
 
 	this.mass = mass;
@@ -52,11 +53,7 @@ public class Solver {
 	this.v = new double[n+1];
 	this.v[0] = v0;
 
-
-
-
-
-
+	this.n = n;
 
 	}
 
@@ -67,6 +64,23 @@ public class Solver {
 	 * を利用してください。
 	 * */
 	public void solve() {
+		//計算で使用する文字をインプットする
+		double m = this.mass;
+		double n = this.n;
+		double dt = this.dt;
+
+		for(int i=0;i<n;i++) {
+
+			//Forceメソッドで計算した値をvalueで取得する
+		    double f = this.F.value(t[i]);
+
+		    //t=i+1の時のt,x,vをそれぞれ計算する
+		    this.t[i+1] = (i+1)*dt;
+		    this.x[i+1] = x[i]+v[i]*dt;
+		    this.v[i+1] = v[i]+f/m*dt;
+
+		}
+
 
 	}
 
